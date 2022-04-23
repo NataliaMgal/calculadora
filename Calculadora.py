0f18c465b856377f41013c761f361a2060fb3b70
206 <206@academicos.cesde.edu.co>
2022-04-22 20:33:45 -0500

    Se crea la clase calculadora

---
+++ Calculadora.py
@@ -0,0 +1,23 @@
+class Calculadora:
+    def __init__(self):
+        self.__numero1=None
+        self.__numero2=None
+    
+    @property
+    def numero1(self):
+        return self.__numero1
+     
+    @property
+    def numero2(self):
+        return self.__numero2
+    
+    @numero1.setter
+    def numero1(self, numero1):
+        self.__numero1=numero1
+
+    @numero2.setter
+    def numero2(self, numero2):
+        self.__numero2=numero2
+    
+    def suma(self):
+        return self.numero1()+self.numero2()
